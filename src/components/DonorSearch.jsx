a3b7fa8a30e1e6f19264b8602e39fc667f073128
TanvirhCSE17 <tanvirhcse17@gmail.com>
2023-11-09 15:39:19 +0600

    minimize DonorSearch

--- src/components/DonorSearch.jsx
+++ src/components/DonorSearch.jsx
@@ -1,61 +1,68 @@
 import React from 'react';
 
-import { Button, Label, Radio, Dropdown } from 'flowbite-react';
+import { Button, Label, Radio, Select } from 'flowbite-react';
 
 const DonorSearch = () => {
     return (
         <div className='container mx-auto md:h-36 my-20  '>
+            <h1 className='text-3xl text-red-600 text-center font-bold p-2'>Search Donor</h1>
             <form className="flex flex-col gap-5 md:mx-20 p-5 md:flex-row justify-around items-center bg-red-600 rounded-lg">
                 <div className="mb-2 block ">
                     <Label className='text-xl text-white' value="Blood Group" />
                     <fieldset className="grid grid-cols-4 max-w-md gap-4">
                         <div className="flex items-center gap-2">
-                            <Radio id="A+" name="countries" value="A+"  />
+                            <Radio id="A+" name="bloodGroup" value="A+" />
                             <Label className=' text-white' htmlFor="A+">A+</Label>
                         </div>
                         <div className="flex items-center gap-2">
-                            <Radio id="B+" name="countries" value="B+" />
+                            <Radio id="B+" name="bloodGroup" value="B+" />
                             <Label className=' text-white' htmlFor="B+">B+</Label>
                         </div>
                         <div className="flex items-center gap-2">
-                            <Radio id="AB+" name="countries" value="AB+" />
+                            <Radio id="AB+" name="bloodGroup" value="AB+" />
                             <Label className=' text-white' htmlFor="AB+">AB+</Label>
                         </div>
                         <div className="flex items-center gap-2">
-                            <Radio id="O+" name="countries" value="O+" />
+                            <Radio id="O+" name="bloodGroup" value="O+" />
                             <Label className=' text-white' htmlFor="O+">O+</Label>
                         </div>
                         <div className="flex items-center gap-2">
-                            <Radio id="A-" name="countries" value="A-"  />
+                            <Radio id="A-" name="bloodGroup" value="A-" />
                             <Label className=' text-white' htmlFor="A-">A-</Label>
                         </div>
                         <div className="flex items-center gap-2">
-                            <Radio id="B-" name="countries" value="B-" />
+                            <Radio id="B-" name="bloodGroup" value="B-" />
                             <Label className=' text-white' htmlFor="B-">B-</Label>
                         </div>
                         <div className="flex items-center gap-2">
-                            <Radio id="AB-" name="countries" value="AB-" />
+                            <Radio id="AB-" name="bloodGroup" value="AB-" />
                             <Label className=' text-white' htmlFor="AB-">AB-</Label>
                         </div>
                         <div className="flex items-center gap-2">
-                            <Radio id="O-" name="countries" value="O-" />
+                            <Radio id="O-" name="bloodGroup" value="O-" />
                             <Label className=' text-white' htmlFor="O-">O-</Label>
                         </div>
                     </fieldset>
                 </div>
                 <div>
-                    <Dropdown outline color='failure' label="Request Area">
-                        <Dropdown.Item>Kushtia</Dropdown.Item>
-                        <Dropdown.Item>Jhinaidah</Dropdown.Item>
-                        <Dropdown.Item>Seikhpara</Dropdown.Item>
-                        <Dropdown.Item>Shailkupa</Dropdown.Item>
-                    </Dropdown>
+                    <div className="">
+                        <Select id="area" required>
+                            <option className='text-lg '>Select Area</option>
+                            <option className='text-lg '>Kushtia</option>
+                            <option className='text-lg '>Jhinaidah</option>
+                            <option className='text-lg '>Sheikhpara</option>
+                            <option className='text-lg '>shailkupa</option>
+                        </Select>
+                    </div>
                 </div>
                 <div>
-                <Dropdown outline color='failure' label="Donor Type">
-                        <Dropdown.Item>All</Dropdown.Item>
-                        <Dropdown.Item>Active</Dropdown.Item>
-                    </Dropdown>
+                    <div className="">
+                        <Select id="donorType" required>
+                            <option className='text-lg '>Donor Type</option>
+                            <option className='text-lg '>All</option>
+                            <option className='text-lg '>Eligible</option>
+                        </Select>
+                    </div>
                 </div>
                 <div><Button outline color='failure' type="submit">Search</Button></div>
             </form>
